3a9f66272fdc3fca891e4404f1651762b268db6f
Jayateertha Guruprasad <jayateertha043@gmail.com>
2021-09-08 17:04:19 +0530

    Fix for Prototype Pollution

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function merge(dst, ...sources) {
     for (src of sources) {
       for (let key in src) {
+        //Check for Prototype Pollution
+        if (key === "__proto__" || key === "constructor") continue;
         let s = src[key], d = dst[key]
         if (Object(s) == s && Object(d) === d) {
           dst[key] = merge(d, s)
